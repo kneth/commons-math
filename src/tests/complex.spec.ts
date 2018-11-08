716eb19f77d5e2067cbd2bf381db59aeadd7fd49
Kenneth Geisshirt <geisshirt@gmail.com>
2018-11-08 17:30:03 +0100

    Update complex tests

--- src/tests/complex.spec.ts
+++ src/tests/complex.spec.ts
@@ -2,19 +2,38 @@ import 'mocha';
 import { expect } from 'chai';
 import { Complex } from '../complex';
 
-describe('Complex numbers', function () {
+describe('Complex numbers', () => {
 
-    it('should construct a complex number', function () {
+    it('should construct a complex number', () => {
         let z = new Complex(1, 2);
         expect(z.re).to.equals(1);
         expect(z.im).to.equals(2);
     });
 
-    it('should add two complex numbers', function () {
+    it('should add two complex numbers', () => {
         let z1 = new Complex(1, 2);
         let z2 = new Complex(3, 4);
         let z3 = z1.add(z2);
         expect(z3.re).to.equals(4);
         expect(z3.im).to.equals(6);
-    })
+    });
+
+    it('should multiply two complex numbers', () => {
+        let z1 = new Complex(1, 2);
+        let z2 = new Complex(3, 4);
+        let z3 = z1.mul(z2);
+        expect(z3.re).to.equals(-5);
+        expect(z3.im).to.equals(10);
+    });
+
+    it('should divide two complex numbers', () => {
+        let z1 = new Complex(1, 2);
+        let z2 = new Complex(3, 4);
+        let z3 = z1.div(z2); // 11/25 + 2/25*i
+        expect(z3.re).to.equals(11.0/25.0);
+        expect(z3.im).to.equals(2.0/25.0);
+
+        let z4 = new Complex(0, 0);
+        expect(() => { z1.div(z4) }).to.throw();
+    });
 })
